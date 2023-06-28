8fa66f723ad27c54629e9eeec3323774f8b81017
prabh <pschhabra22@gmail.com>
2023-06-28 00:52:52 -0400

    Created minimal structure for pdf file

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+files=glob.glob('excel_files/*.xlsx')
+
+for filepath in files:
+    file_1=pd.read_excel(filepath)
+    pdf=FPDF(orientation="P",unit='mm',format='A4')
+    pdf.add_page()
+    path=Path(filepath).stem
+    invoice_nr=path.split('-')[0]
+    pdf.set_font(family="Times",size=16,style="B")
+    pdf.cell(w=50,h=8,txt=f"Invoice nr. {invoice_nr}")
+    pdf.output(f"PDFs/{path}.pdf")
 
 
-file_1=pd.read_excel("excel_files/10001-2023.1.18.xlsx")
-print(file_1)
