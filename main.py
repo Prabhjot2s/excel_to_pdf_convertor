7f9ef90ae662bb9b261a72692e495fe7cae2382f
prabh <pschhabra22@gmail.com>
2023-06-29 16:59:47 -0400

    June 29 : Completed project for excel to pdf convertor

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fpdf import FPDF
 from pathlib import Path
 
 files = glob.glob('excel_files/*.xlsx')
-
+total_price = 0
 for filepath in files:
     file_1 = pd.read_excel(filepath)
     pdf = FPDF(orientation="P", unit='mm', format='A4')
@@ -16,17 +16,19 @@ for filepath in files:
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Date {path.split('-')[1]}", ln=1)
-    total_price = 0
 
-#Add a header
+
+    # Add a header
     header = list(file_1.columns)
 
     pdf.set_font(family="Times", size=10, style="B")
-    pdf.cell(w=30, h=8, txt=header[0].replace('_',' '), border=1)
-    pdf.cell(w=70, h=8, txt=header[1].replace('_',' '), border=1)
-    pdf.cell(w=30, h=8, txt=header[2].replace('_',' '), border=1)
-    pdf.cell(w=30, h=8, txt=header[3].replace('_',' '), border=1)
-    pdf.cell(w=30, h=8, txt=header[4].replace('_',' '), border=1, ln=1)
+    pdf.cell(w=30, h=8, txt=header[0].replace('_', ' '), border=1)
+    pdf.cell(w=70, h=8, txt=header[1].replace('_', ' '), border=1)
+    pdf.cell(w=30, h=8, txt=header[2].replace('_', ' '), border=1)
+    pdf.cell(w=30, h=8, txt=header[3].replace('_', ' '), border=1)
+    pdf.cell(w=30, h=8, txt=header[4].replace('_', ' '), border=1, ln=1)
+
+
 #Add Rows to the table
     for index, row in file_1.iterrows():
         pdf.set_font(family="Times", size=10, style="")
@@ -38,6 +40,6 @@ for filepath in files:
         total_price += row['total_price']
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=30, h=8, txt=f"Total Price-{total_price}", ln=1)
+    pdf.cell(w=30, h=8, txt=f"Total Due AMount is-{total_price}", ln=1)
 
     pdf.output(f"PDFs/{path}.pdf")
